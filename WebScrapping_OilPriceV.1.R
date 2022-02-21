9cc8a2c231061c67eeedb28c226f94cc67875a90
Pers Thanachot <78893367+PersThanachot@users.noreply.github.com>
2022-02-21 11:25:46 +0700

    Update WebScrapping_OilPriceV.1.R

--- WebScrapping_OilPriceV.1.R
+++ WebScrapping_OilPriceV.1.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(XML)
 library(rvest)
 
+#Link For Web Scrapping
 Oil65 <- read_html('https://www.bangchak.co.th/th/oilprice/historical')
 Oil64 <- read_html('https://www.bangchak.co.th/th/oilprice/historical?year=2021')
 Oil63 <- read_html('https://www.bangchak.co.th/th/oilprice/historical?year=2020')
@@ -11,11 +12,10 @@ Oil60 <- read_html('https://www.bangchak.co.th/th/oilprice/historical?year=2017'
 Oil59 <- read_html('https://www.bangchak.co.th/th/oilprice/historical?year=2016')
 Oil58 <- read_html('https://www.bangchak.co.th/th/oilprice/historical?year=2015')
 
-# https://www.bangchak.co.th/th/oilprice/historical?year=2021
-
-tables65 <- Oil65 %>% html_table()
-OilPrice65 <- data.frame(tables65[1])
-OilPrice65 <- OilPrice65[-c(1),]
+#Import to Dataframe 
+tables65 <- Oil65 %>% html_table() #rvest find all tables
+OilPrice65 <- data.frame(tables65[1]) #Select some table -> 1,2,3,...,N
+OilPrice65 <- OilPrice65[-c(1),] #Remove first row
 
 tables64 <- Oil64 %>% html_table()
 OilPrice64 <- data.frame(tables64[1])
@@ -45,6 +45,7 @@ tables58 <- Oil58 %>% html_table()
 OilPrice58 <- data.frame(tables58[1])
 OilPrice58 <- OilPrice58[-c(1),]
 
+#Combine all Dataframe with rows
 OilPrice <- rbind(OilPrice65,
                   OilPrice64,
                   OilPrice63,
@@ -54,8 +55,20 @@ OilPrice <- rbind(OilPrice65,
                   OilPrice59,
                   OilPrice58)
 
+#Rename column
 names(OilPrice)[1] <- 'Date'
 names(OilPrice)[2] <- 'Premium_Diesel_B7'
+names(OilPrice)[3] <- 'Diesel_B7'
+names(OilPrice)[4] <- 'Diesel'
+names(OilPrice)[5] <- 'Diesel_B20'
+names(OilPrice)[6] <- 'E85'
+names(OilPrice)[7] <- 'E20'
+names(OilPrice)[8] <- '91'
+names(OilPrice)[9] <- '95'
+names(OilPrice)[10] <- 'NGV'
+
+#Output Dataframe to excel.csv
+write.csv(OilPrice,"C:/Users/Noter/Desktop/Data/OilPrice/OilPrice58_65.csv", row.names = FALSE)
 names(OilPrice)[3] <- 'Diesel_B7'
 names(OilPrice)[4] <- 'Diesel'
 names(OilPrice)[5] <- 'Diesel_B20'
